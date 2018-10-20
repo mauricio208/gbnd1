39f8b6a9d6c74ac2956d729aa3b9bb4d9872a582
Mauricio Machuca <mauricio208@gmail.com>
2018-10-19 23:22:23 -0400

    adding env variable support

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ var cors = require('cors');
 var app = express();
 
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/growthbondtest", { useNewUrlParser: true });
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
